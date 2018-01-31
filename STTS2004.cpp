18c1fdd0941ef2d6f84ac7489d7c945ac2e8310c
chisl <info@chisl.io>
2018-01-31 19:09:26 +0100

    Initial commit

---
+++ STTS2004.cpp
@@ -0,0 +1,18 @@
+/*
+ * name:        STTS2004
+ * description: 2.2 V memory module temperature sensor with a 4 Kb SPD EEPROM
+ * manuf:       ST Microelectronics
+ * version:     Version 0.1
+ * url:         http://www.st.com/resource/en/datasheet/stts2004.pdf
+ * date:        2018-01-31
+ * author       https://chisl.io/
+ * file:        STTS2004.cpp
+ */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+
+#include "STTS2004.hpp"
+
